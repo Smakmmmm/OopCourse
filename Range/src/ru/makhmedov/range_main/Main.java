63ceb5edd2b38c7da685719b2747626c405c63dd
J.A.R.V.I.S <code4comf@gmail.com>
2023-01-30 00:19:30 +0700

    add IDEA project, add new module "Range"

--- Range/src/ru/makhmedov/range_main/Main.java
+++ Range/src/ru/makhmedov/range_main/Main.java
@@ -1,4 +1,40 @@
 package ru.makhmedov.range_main;
 
+import ru.makhmedov.range.Range;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Range range = new Range(10, 100);
+
+        System.out.println("Длина диапазона равна: " + range.getLength());
+
+        System.out.println("Введите число:");
+        double number = scanner.nextDouble();
+
+        if (range.isInside(number)) {
+            System.out.println("Число лежит в заданном диапазоне.");
+        } else {
+            System.out.println("Число лежит вне диапазона.");
+        }
+
+        System.out.println("Задайте новый диапазон. Начало диапазона:");
+        range.setFrom(scanner.nextDouble());
+
+        System.out.println("Введите конец диапазона:");
+        range.setTo(scanner.nextDouble());
+
+        System.out.println("Теперь начало диапазона: " + range.getFrom() + " и конец: " + range.getTo());
+        System.out.println("Длина заданного Вами диапазона равна: " + range.getLength());
+
+        System.out.println("Введите новое число:");
+        number = scanner.nextDouble();
+
+        if (range.isInside(number)) {
+            System.out.println("Число лежит в заданном диапазоне.");
+        } else {
+            System.out.println("Число лежит вне диапазона.");
+        }
+    }
 }
