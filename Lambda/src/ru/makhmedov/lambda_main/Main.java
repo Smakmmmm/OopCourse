c3b531d54df4d5e280057eb6988023ae171a8e53
J.A.R.V.I.S <code4comf@gmail.com>
2023-04-04 23:12:31 +0700

    Corrected some mistakes in the "Lambda" task.

--- Lambda/src/ru/makhmedov/lambda_main/Main.java
+++ Lambda/src/ru/makhmedov/lambda_main/Main.java
@@ -1,41 +1,47 @@
 package ru.makhmedov.lambda_main;
 
-import ru.makhmedov.person.Person;
+import ru.makhmedov.lambda_person.Person;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        List<Person> persons = new ArrayList<>();
-        persons.add(new Person("Максим", 26));
-        persons.add(new Person("Денис", 20));
-        persons.add(new Person("Дмитрий", 33));
-        persons.add(new Person("Виктория", 18));
-        persons.add(new Person("Денис", 15));
-        persons.add(new Person("Иван", 45));
+        List<Person> persons = new ArrayList<>(Arrays.asList(new Person("Максим", 26),
+                new Person("Денис", 20),
+                new Person("Дмитрий", 33),
+                new Person("Виктория", 18),
+                new Person("Денис", 15),
+                new Person("Иван", 45)));
 
-        List<String> uniqueNames = persons.stream().map(Person::getName).distinct().toList();
+        List<String> uniqueNames = persons.stream()
+                .map(Person::name)
+                .distinct()
+                .toList();
         System.out.println(uniqueNames);
 
-        String uniqueNamesString = persons.stream().map(Person::getName)
-                .distinct().collect(Collectors.joining(", ", "Имена: ", "."));
+        String uniqueNamesString = uniqueNames.stream()
+                .collect(Collectors.joining(", ", "Имена: ", "."));
         System.out.println(uniqueNamesString);
 
-        List<Person> personsOver18 = persons.stream().filter(p -> (p.getAge() > 18)).toList();
-        personsOver18.forEach(s -> System.out.print(s.getName() + " "));
+        List<Person> personsUnder18 = persons.stream()
+                .filter(p -> p.age() < 18)
+                .toList();
+        personsUnder18.forEach(s -> System.out.print(s.name() + " "));
         System.out.println();
 
-        double averageAge = personsOver18.stream().mapToDouble(Person::getAge).summaryStatistics().getAverage();
-        System.out.println("Средний возраст для людей старше 18: " + averageAge);
+        double averageAge = personsUnder18.stream()
+                .collect(Collectors.averagingInt(Person::age));
+        System.out.println("Средний возраст для людей младше 18: " + averageAge);
 
-        Map<String, Double> personsByName = persons.stream()
-                .collect(Collectors.groupingBy(Person::getName, Collectors.averagingDouble(Person::getAge)));
-        System.out.println(personsByName);
+        Map<String, Double> averageAgeByName = persons.stream()
+                .collect(Collectors.groupingBy(Person::name, Collectors.averagingInt(Person::age)));
+        System.out.println(averageAgeByName);
 
         System.out.println("Люди возрастом от 20 до 45:");
-        persons.stream().
-                filter(p -> (p.getAge() >= 20 && p.getAge() <= 45))
-                .sorted(Comparator.comparingInt(Person::getAge)).forEach(s -> System.out.print(s.getName() + " "));
+        persons.stream()
+                .filter(p -> p.age() >= 20 && p.age() <= 45)
+                .sorted((p1, p2) -> p2.age() - p1.age())
+                .forEach(s -> System.out.print(s.name() + " "));
     }
 }
---
+++ Lambda/src/ru/makhmedov/lambda_person/Person.java
@@ -0,0 +1,4 @@
+package ru.makhmedov.lambda_person;
+
+public record Person(String name, int age) {
+}
